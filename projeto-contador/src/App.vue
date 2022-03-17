1d868225886bf39c5c5b5bdf7dde5770c0008447
Maria Eugênia Da Cruz <mariaebc21@gmail.com>
2022-03-17 11:36:11 -0300

    update readme

---
+++ projeto-contador/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+    <ProjContador/>
+  </div>
+</template>
+
+<script>
+import ProjContador from './components/ProjContador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProjContador,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ projeto-contador/src/components/ProjContador.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="Novo">
+    <h1>Contador</h1>
+    <div class="sub">
+      <P>Clique para subtrair: </P><button v-on:click="Subtrair">-</button>
+    </div>
+    <div class="sum">
+      <p>Clique para somar: </p>
+      <button v-on:click="Somar">+</button>
+    </div>
+
+    <p><strong>Valor atual:</strong> {{ mensagem }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjContador",
+  data() {
+    return {
+      mensagem: 0,
+    };
+  },
+  methods: {
+    Somar() {
+      this.mensagem = this.mensagem + 1;
+    },
+    Subtrair() {
+      this.mensagem = this.mensagem - 1;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sub {
+  display: flex;
+  padding: 5px;
+ justify-content: center;
+}
+.sum {
+    padding: 5px;
+    display: flex;
+    justify-content: center;
+}
+</style>
